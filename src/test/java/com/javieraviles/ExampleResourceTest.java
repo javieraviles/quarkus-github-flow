74a901410c234293740fed6caf7e269e22d7831f
Javier Aviles <avileslopez.javier@gmail.com>
2020-05-31 01:32:47 +0200

    Fixing unit tests

--- src/test/java/com/javieraviles/ExampleResourceTest.java
+++ src/test/java/com/javieraviles/ExampleResourceTest.java
@@ -3,19 +3,19 @@ package com.javieraviles;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.inject.Inject;
 
 @QuarkusTest
 public class ExampleResourceTest {
 
+    @Inject
+    ExampleResource exampleResource;
+
     @Test
     public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+        assertEquals("hello",exampleResource.hello());
     }
 
 }
